8ed4a98dbd9d033410c281addce1cfb34dd90cea
IvanSSantana <ivansantospriv@gmail.com>
2025-08-04 11:36:53 -0300

    Criando home page.

--- pokedex/components/Header/index.jsx
+++ pokedex/components/Header/index.jsx
@@ -8,4 +8,26 @@ const imageSource = require("@/assets/images/header.jpg");
 export default function Header({ title, back = false }) {
     const router = useRouter();
 
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image source={imageSource} style={styles.image} />
+            </View>
+
+            <View style={styles.titleContainer}>
+                <View style={{ flexDirection: "row", alignItems: "center"}}>
+                    {
+                        back &&
+                        <Pressable onPress={() => router.back()} style={styles.backButton}>
+                            <Ionicons name="arrow-back" size={28} color="black" />
+                        </Pressable>
+                    }
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+                <Text style={{ fontSize: 16 }}>
+                    {!back ? "Encontre seu pokémon favorito aqui." : "Aqui está seu pokémon."}
+                </Text>
+            </View>
+        </View>
+    );
 }
